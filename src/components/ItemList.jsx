4d3fb7fdc274a5c94a650bc408507b6abc138d5b
Official-MahnoorTariq <real.mahnoor.tariq@gmail.com>
2026-04-29 18:39:19 +0500

    Add files via upload

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,80 @@
+import Select from "react-select";
+import EmptyView from "./Emptyview";
+import {useMemo, useState } from "react";
+import { useItemsStore } from "../stores/itemsStore";
+;
+
+const sortingOptions = [
+  {
+    label: "Sort by default",
+    value: "default",
+  },
+   {
+    label: "Sort by packed",
+    value: "packed",
+  },
+   {
+    label: "Sort by unpacked",
+    value: "unpacked",
+  },
+];
+
+export default function ItemList() {
+  const items = useItemsStore((state) => state.items);
+    const deleteItem = useItemsStore((state) => state.deleteItem);
+    const toggleItem = useItemsStore((state) => state.toggleItem);
+  const [sortBy,setSortBy] = useState("default");
+ 
+const sortedItems = useMemo(() => {
+  const itemsCopy = [...items];
+
+  if (sortBy === "packed") {
+    return itemsCopy.sort((a, b) => b.packed - a.packed);
+  }
+
+  if (sortBy === "unpacked") {
+    return itemsCopy.sort((a, b) => a.packed - b.packed);
+  }
+
+  return itemsCopy;
+}, [items, sortBy]);
+  return (
+    <ul className="item-list">
+   {sortedItems.length === 0 ? <EmptyView /> : null}
+
+     {sortedItems.length > 0 ? (
+  <section className="sorting">
+    <Select
+      onChange={(option) => setSortBy(option.value)}
+      defaultValue={sortingOptions[0]}
+      options={sortingOptions}
+    />
+  </section>
+) : null}  
+
+
+      {
+        sortedItems.map((item) => {
+          return(
+          <Item key={item.id} item={item}
+           onDeleteItem={deleteItem}
+        onToggleItem={toggleItem}
+          />
+          );
+        })
+      }
+    </ul>
+  );
+}
+
+function Item ({item,onDeleteItem,onToggleItem}){
+  return(
+<li className="item">
+  <label>
+    <input onChange={ () => onToggleItem(item.id)} checked={item.packed} type="checkbox" />
+     {item.name}
+  </label>
+  <button onClick={() => onDeleteItem(item.id)}>❌</button>
+</li>
+  );
+}
